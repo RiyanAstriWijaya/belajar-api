81707b99412b2f4faae12bab3bd4c25f8b2dda9b
RiyanAstriWijaya <thekingofkelassore@gmail.com>
2024-06-14 08:56:11 +0700

    push

--- controller/user.js
+++ controller/user.js
@@ -1,22 +1,23 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const addData = async (req, res, next) => {
-  try {
-    const body = req.body;
-    const user = await prisma.user.create({
-      data: {
-        username: body.username,
-        password: body.password,
-      },
-    });
-    return res.status(201).json({
-      status: true,
-      message: "data berhasil di input",
-      data: user,
-    });
-  } catch (error) {
-    next(error);
-  }
+module.exports = {
+  addData: async (req, res, next) => {
+    try {
+      const body = req.body;
+      const user = await prisma.user.create({
+        data: {
+          username: body.username,
+          password: body.password,
+        },
+      });
+      return res.status(201).json({
+        status: true,
+        message: "data berhasil di input",
+        data: user,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
 };
-module.exports = addData;
